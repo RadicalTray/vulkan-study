06061f60e529b4d9d1192023d0e755a5ead103b4
RadicalTray <hydrochloricmahn@gmail.com>
2024-11-02 23:42:16 +0700

    staging buffer

--- src/main.cpp
+++ src/main.cpp
@@ -807,6 +807,31 @@ private:
     }
   }
 
+  void createVertexBuffer() {
+    VkDeviceSize bufSize = sizeof(vertices[0]) * vertices.size();
+    VkBuffer stagingBuf;
+    VkDeviceMemory stagingBufMem;
+    createBuffer(bufSize,
+                 VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                 stagingBuf,
+                 stagingBufMem);
+
+    void *pData;
+    vkMapMemory(device, stagingBufMem, 0, bufSize, 0, &pData);
+    memcpy(pData, vertices.data(), (size_t)bufSize);
+    vkUnmapMemory(device, stagingBufMem);
+
+    createBuffer(bufSize,
+                 VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+                 VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                 vertexBuffer,
+                 vertexBufferMem);
+    copyBuffer(stagingBuf, vertexBuffer, bufSize);
+    vkDestroyBuffer(device, stagingBuf, nullptr);
+    vkFreeMemory(device, stagingBufMem, nullptr);
+  }
+
   void createBuffer(const VkDeviceSize size,
                     const VkBufferUsageFlags usage,
                     const VkMemoryPropertyFlags properties,
@@ -838,17 +863,39 @@ private:
     vkBindBufferMemory(device, buffer, memory, 0);
   }
 
-  void createVertexBuffer() {
-    VkDeviceSize bufSize = sizeof(vertices[0]) * vertices.size();
-    createBuffer(bufSize,
-                 VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-                 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-                 vertexBuffer,
-                 vertexBufferMem);
-    void *pData;
-    vkMapMemory(device, vertexBufferMem, 0, bufSize, 0, &pData);
-    memcpy(pData, vertices.data(), (size_t)bufSize);
-    vkUnmapMemory(device, vertexBufferMem);
+  void copyBuffer(VkBuffer srcBuf, VkBuffer dstBuf, VkDeviceSize size) {
+    VkCommandBufferAllocateInfo allocInfo = {
+      .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+      .commandPool = commandPool,
+      .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+      .commandBufferCount = 1,
+    };
+
+    VkCommandBuffer commandBuffer;
+    vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+
+    VkCommandBufferBeginInfo beginInfo = {
+      .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+      .flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
+    };
+    vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+    VkBufferCopy copyRegion = {
+      .size = size,
+    };
+    vkCmdCopyBuffer(commandBuffer, srcBuf, dstBuf, 1, &copyRegion);
+
+    vkEndCommandBuffer(commandBuffer);
+
+    VkSubmitInfo submitInfo = {
+      .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+      .commandBufferCount = 1,
+      .pCommandBuffers = &commandBuffer,
+    };
+    vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+
+    vkQueueWaitIdle(graphicsQueue);
+    vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
   }
 
   uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
